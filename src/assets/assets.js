b52468298d345b64e642c9206c7a720e7a951100
Arina Saffanah Zakiyyah <arinasaffanahzzakiyyah4@gmail.com>
2025-12-09 11:49:17 +0700

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg'
+import img_mr from './img_mr.png'
+import img_er from './img_er.png'
+import img_bp from './img_bp.png'
+import header_img from './header_img.png'
+import menu_icon from './menu_icon.svg'
+import cross_icon from './cross_icon.svg'
+
+export const assets = {
+    logo,
+    header_img,
+    menu_icon,
+    cross_icon
+}
+
+export const workPrograms = [
+    {
+        title: "Market Research",
+        supervisor: "Arins",
+        pic: "Grace",
+        image: img_mr
+    },
+    {
+        title: "Evaluation Research",
+        supervisor: "Arins",
+        pic: "Rani",
+        image: img_er
+    },
+    {
+        title: "Blueprint",
+        supervisor: "Arins",
+        pic: "Stefi",
+        image: img_bp
+    }
+]
+
+export const feedbackPrograms = [
+    {
+        division: "HR",
+        text: "So good!"
+    },
+    {
+        division: "Operations",
+        text: "So nice!"
+    },
+    {
+        division: "Finance",
+        text: "So cool!"
+    }
+]
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id='About'>
+        <h1>About <span>Our Programs</span></h1>
+        <p>Passionate about research, Dedicate to support insight</p>
+    </div>
+  )
+}
+
+export default About
