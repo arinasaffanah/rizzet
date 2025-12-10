b733082399deb1917da7984999ea1a0006e7dc88
arins <arina@gmail.com>
2025-12-10 23:41:02 +0700

    Add programs section

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -6,34 +6,71 @@ import header_img from './header_img.png'
 import menu_icon from './menu_icon.svg'
 import cross_icon from './cross_icon.svg'
 import brand_img from './brand_img.png'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+import project_img_1 from './project_img_1.jpg'
+import project_img_2 from './project_img_2.jpg'
+import project_img_3 from './project_img_3.jpg'
 
 export const assets = {
     logo,
     header_img,
     menu_icon,
     cross_icon,
-    brand_img
+    brand_img,
+    left_arrow,
+    right_arrow
 }
 
 export const workPrograms = [
     {
         title: "Market Research",
-        supervisor: "Arins",
+        supervisor: "Arina",
         pic: "Grace",
-        image: img_mr
+        image: project_img_1
     },
     {
         title: "Evaluation Research",
-        supervisor: "Arins",
+        supervisor: "Arina",
         pic: "Rani",
-        image: img_er
+        image: project_img_2
     },
     {
         title: "Blueprint",
-        supervisor: "Arins",
+        supervisor: "Arina",
         pic: "Stefi",
-        image: img_bp
-    }
+        image: project_img_3
+    },
+    {
+        title: "Annual Research",
+        supervisor: "Arina",
+        pic: "Alvin",
+        image: project_img_1
+    },
+    {
+        title: "Content Research",
+        supervisor: "Khayla",
+        pic: "Louisa",
+        image: project_img_2
+    },
+    {
+        title: "SxC Grow",
+        supervisor: "Khayla",
+        pic: "Bella",
+        image: project_img_3
+    },
+    {
+        title: "Dashboard",
+        supervisor: "Ilmi",
+        pic: "Ilmi",
+        image: project_img_2
+    },
+    {
+        title: "Data Central",
+        supervisor: "Arina",
+        pic: "Arina",
+        image: project_img_3
+    },
 ]
 
 export const feedbackPrograms = [
---
+++ src/components/Programs.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { assets, workPrograms } from '../assets/assets'
+
+const Programs = () => {
+  return (
+    <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Programs'>
+        <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Work Programs <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
+        <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Analyze data, Provide Insight to Support Decision Making-Explore Our Portfolio</p>
+
+        {/* slider buttons */}
+
+        <div className='flex justify-end items-center mb-8'>
+            <button className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous Project'>
+                <img src={assets.left_arrow} alt="Previous" />
+            </button>
+            <button className='p-3 bg-gray-200 rounded mr-2' aria-label='Next Project'>
+                <img src={assets.right_arrow} alt="Next" />
+            </button>
+        </div>
+
+        {/* programs slider container */}
+
+        <div className='overflow-hidden'>
+            <div className='flex gap-8 transition-transform duration-500 ease-in-out'>
+                {workPrograms.map((program, index)=>(
+                    <div key={index} className='relative shrink-0 w-full sm:w-1/4'>
+                        <img src={program.image} alt={program.title} className='w-full h-auto mb-14' />
+                        <div className='absolute left-0 right-0 bottom-5 flex justify-center'>
+                            <div className='inline-block bg-white w-3/4 px-4 py-2 shadow-md'>
+                                <h2 className='text-xl font-semibold text-gray-800 '>{program.title}</h2>
+                                <p className='text-sm text-gray-500'>
+                                    {program.pic} (PIC)<span></span>
+                                </p>
+                                <p className='text-xs text-gray-500'>
+                                    {program.supervisor} (Supervisor)<span></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Programs
